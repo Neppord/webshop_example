e57c2ff173b3ae54343832fd79d8c42da4bd44ae
Samuel Ytterbrink <neppord@gmail.com>
2014-11-12 17:39:41 +0100

    adds static css and improves looks.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static(__dirname + "/static"));
+
 app.get("/", function (req, res) {
-  res.send("hello world!")
+  res.send("<html><head><link rel='stylesheet' href='/main.css'></head><body><h1>Hello World!</h1></body></html>");
 });
 
 app.listen(8080);
