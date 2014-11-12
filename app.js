1a665f1d618648d86a3af78af720538b0c33fbde
Samuel Ytterbrink <neppord@gmail.com>
2014-11-12 17:49:52 +0100

    moves the hello world html inot a jade template.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + "/static"));
 
 app.get("/", function (req, res) {
-  res.send("<html><head><link rel='stylesheet' href='/main.css'></head><body><h1>Hello World!</h1></body></html>");
+  res.render("index.jade");
 });
 
 app.listen(8080);
